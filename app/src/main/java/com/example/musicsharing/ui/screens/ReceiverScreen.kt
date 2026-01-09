9dedf5845d4d2d994f8e675ae240362d398a55c3
Himanshu Mishra <himanshumishra@Himanshus-MacBook-Air.local>
2026-01-09 20:29:30 +0530

    Yet not snycronised and Proper sending of audio file over the audio

--- app/src/main/java/com/example/musicsharing/ui/screens/ReceiverScreen.kt
+++ app/src/main/java/com/example/musicsharing/ui/screens/ReceiverScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.musicsharing.utils.MusicServerInfo
 import com.example.musicsharing.ui.receiver.ReceiverStatus
